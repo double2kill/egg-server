083d696dcb95b541bd34a996b07373c5d64436c4
double2kill <379563000@qq.com>
2019-01-05 15:02:47 +0800

    修改时间为获取一年前到今天

--- web/src/components/sshInfo/helper.js
+++ web/src/components/sshInfo/helper.js
@@ -1,67 +1,70 @@
 const echarts = require('echarts')
+const moment = require('moment')
 
 function getVirtulData(data) {
-    data = data.map(item => echarts.format.formatTime('yyyy-MM-dd', item.time))
-    data = data.reduce((result, item) => {
-        const newItem = [item, 1]
-        if(result.length === 0) {
-            result.push(newItem)
-            return result
-        }
-        const last = result[result.length - 1]
-        if(last[0] === item){
-            last[1] += 1
-            return result
-        } else {
-            result.push(newItem)
-            return result
-        }
-    }, [])
+  data = data.map(item => echarts.format.formatTime('yyyy-MM-dd', item.time))
+  data = data.reduce((result, item) => {
+    const newItem = [item, 1]
+    if (result.length === 0) {
+      result.push(newItem)
+      return result
+    }
+    const last = result[result.length - 1]
+    if (last[0] === item) {
+      last[1] += 1
+      return result
+    } else {
+      result.push(newItem)
+      return result
+    }
+  }, [])
   return data;
 }
 
 export function getOptions(data, user) {
-
-const option = {
+  const lastYearToday = moment().add(-1, 'year').format('YYYY-MM-DD')
+  const today = moment().format('YYYY-MM-DD')
+  const range = [lastYearToday, today]
+  const option = {
     title: {
-        top: 30,
-        left: 'center',
-        text: `${user}的ssh登陆频率图`
+      top: 30,
+      left: 'center',
+      text: `${user}的ssh登陆频率图`
     },
-    tooltip : {},
+    tooltip: {},
     visualMap: {
-        min: 0,
-        max: 10,
-        type: 'piecewise',
-        orient: 'horizontal',
-        left: 'center',
-        top: 65,
-        textStyle: {
-            color: '#000'
-        }
+      min: 0,
+      max: 10,
+      type: 'piecewise',
+      orient: 'horizontal',
+      left: 'center',
+      top: 65,
+      textStyle: {
+        color: '#000'
+      }
     },
     calendar: {
-        top: 120,
-        left: 30,
-        right: 30,
-        cellSize: ['auto', 13],
-        range: '2018',
-        itemStyle: {
-            normal: {borderWidth: 0.5}
-        },
-        yearLabel: {show: false},
-        monthLabel: {
-            nameMap: 'cn',
-        },
-        dayLabel: {
-            firstDay: 1,
-            nameMap: ['周日', '周一', '周二', '周三', '周四', '周五', '周六']
-        },
+      top: 120,
+      left: 30,
+      right: 30,
+      cellSize: ['auto', 13],
+      range,
+      itemStyle: {
+        normal: { borderWidth: 0.5 }
+      },
+      yearLabel: { show: false },
+      monthLabel: {
+        nameMap: 'cn',
+      },
+      dayLabel: {
+        firstDay: 1,
+        nameMap: ['周日', '周一', '周二', '周三', '周四', '周五', '周六']
+      },
     },
     series: {
-        type: 'heatmap',
-        coordinateSystem: 'calendar',
-        data: getVirtulData(data)
+      type: 'heatmap',
+      coordinateSystem: 'calendar',
+      data: getVirtulData(data)
     }
   };
   return option
