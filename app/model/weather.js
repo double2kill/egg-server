139e7acb106efc0a66f5fb07a9981be95cc3f59d
double2kill <379563000@qq.com>
2019-11-26 16:50:02 +0800

    feat: 🎸 modify mongoose schema

--- app/model/weather.js
+++ app/model/weather.js
@@ -4,10 +4,15 @@ module.exports = app => {
   const mongoose = app.mongoose;
   const Schema = mongoose.Schema;
 
-  const UserSchema = new Schema({
-    statistics: Object,
+  const WeatherSchema = new Schema({
+    text: String,
     date: String,
+    city: String,
+    data: [],
+    statistics: {},
+    users: [],
+    create_time: { type: Date, default: Date.now },
   });
 
-  return mongoose.model('Weather', UserSchema);
+  return mongoose.model('Weather', WeatherSchema);
 };
