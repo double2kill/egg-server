17c7ec14f062c58909d0b5fa96e8a0223755194b
double2kill <379563000@qq.com>
2020-02-12 23:10:05 +0800

    feat: 🎸 use new mobile-ui vant

--- web/components/textStorage/index.vue
+++ web/components/textStorage/index.vue
@@ -1,62 +1,83 @@
 <template>
-  <md-field>
-    <md-textarea-item
-      v-model="text"
-      :autosize="true"
-      :rows="1"
-      clearable
-      placeholder="请输入文字"
-      :max-height="500"
+  <div>
+    <van-nav-bar
+      title="文字收集仓"
     />
-    <md-button class="submit-button" inline size="small" @click="handleCopy()">
-      复制
-    </md-button>
-    <md-button class="submit-button" type="primary" inline size="small" @click="submitText()">
-      提交
-    </md-button>
-  </md-field>
+    <div>
+      <van-cell-group>
+        <van-field v-model="storageName" placeholder="请输入你喜欢的仓库名" label="仓库名">
+          <van-button slot="button" size="small" @click="handleChangeStorageName">
+            修改
+          </van-button>
+        </van-field>
+        <van-field
+          v-model="text"
+          placeholder="请输入文字"
+          label="仓库文字"
+          rows="1"
+          autosize
+          type="textarea"
+          clearable
+        />
+      </van-cell-group>
+      <div class="submit-group">
+        <van-button @click="handleCopy()">
+          复制
+        </van-button>
+        <van-button type="primary" @click="submitText()">
+          提交
+        </van-button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { Button, TextareaItem, Field, Toast } from 'mand-mobile'
-
+import { Toast } from 'vant'
 import axios from 'axios'
 import URL from '../../constants'
 
 const SPAIR = URL.SPAIR
 
+const defaultStorageName = '默认仓库'
+const TEXT_STORAGE_STORAGE_NAME = 'textStorage.storageName'
+
 export default {
   name: 'TextStorage',
-  components: {
-    [Button.name]: Button,
-    [TextareaItem.name]: TextareaItem,
-    [Field.name]: Field
-  },
   data() {
     return {
+      storageName: defaultStorageName,
       text: ''
     }
   },
-  created() {
+  mounted() {
+    const storageName = localStorage.getItem(TEXT_STORAGE_STORAGE_NAME) || defaultStorageName
+    this.storageName = storageName
     this.getText()
   },
   methods: {
+    getStorageName() {
+      return this.storageName || defaultStorageName
+    },
     async getText() {
+      const storageName = this.getStorageName()
       try {
-        const res = await axios.get(`${SPAIR}/ns/liuchen`)
+        const res = await axios.get(`${SPAIR}/textStorage/${storageName}`)
         this.text = decodeURIComponent(res.data)
       } catch (error) {
-        Toast.succeed('服务器出错')
+        Toast.fail('服务器出错')
+        throw new Error('服务器出错')
       }
     },
     async submitText() {
       try {
         // encodeURIComponent twice
+        const storageName = this.getStorageName()
         const text = encodeURIComponent(encodeURIComponent(this.text))
-        await axios.get(`${SPAIR}/ns/liuchen/${text}`)
-        Toast.succeed('保存成功')
+        await axios.get(`${SPAIR}/textStorage/${storageName}/${text}`)
+        Toast.success('保存成功')
       } catch (error) {
-        Toast.succeed('保存失败')
+        Toast.fail('保存失败')
       }
     },
     handleCopy() {
@@ -66,15 +87,23 @@ export default {
       tempInput.select()
       document.execCommand('copy')
       document.body.removeChild(tempInput)
-      Toast.succeed('复制成功')
+      Toast.success('复制成功')
+    },
+    async handleChangeStorageName() {
+      await this.getText()
+      localStorage.setItem(TEXT_STORAGE_STORAGE_NAME, this.storageName)
     }
   }
 }
 
 </script>
 
-<style scoped>
-  .submit-button {
-    margin-top: 15px;
+<style>
+  .van-cell {
+    align-items: center;
+  }
+  .submit-group {
+    padding: 16px;
+    text-align: right;
   }
 </style>
--- web/plugins/element-ui.js
+++ web/plugins/element-ui.js
@@ -5,7 +5,11 @@ import locale from 'element-ui/lib/locale/lang/en'
 import mandMobile from 'mand-mobile'
 import 'mand-mobile/lib/mand-mobile.css'
 
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
 export default () => {
   Vue.use(Element, locale)
   Vue.use(mandMobile)
+  Vue.use(Vant)
 }
