ffebb828cb02f890c1393b8338fef369b7dc79ee
double2kill <379563000@qq.com>
2022-03-08 22:30:07 +0800

    feat(modify footer): modify footer

--- web/layouts/footer.vue
+++ web/layouts/footer.vue
@@ -1,10 +1,10 @@
 <template>
   <footer>
     <el-divider />
-    <p>Copyright © 2019-{{ currentYear }}. All Rights Reserved. 连学军/刘晨 版权所有</p>
+    <p>Copyright © 2019-{{ currentYear }}. All Rights Reserved. 连学军 版权所有</p>
     <p>
-      <el-link href="http://www.beian.miit.gov.cn" target="_blank">
-        闽ICP备18009406号
+      <el-link href="https://beian.miit.gov.cn/#/Integrated/index" target="_blank">
+        闽ICP备2022002919号-1
       </el-link>
     </p>
   </footer>
