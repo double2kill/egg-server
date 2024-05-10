4fc7e2ae997186d149eb3dc51a23d2aaf223e9e7
double2kill <379563000@qq.com>
2024-05-10 13:00:47 +0800

    fix ecosymstem

--- web/ecosystem.config.cjs
+++ web/ecosystem.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
       port: "8000",
       exec_mode: "cluster",
       instances: "max",
-      script: "node ./.output/server/index.mjs",
+      script: "./.output/server/index.mjs",
     },
   ],
 };
