396a602bf01455d86dbb5f2c19551ab07544e898
double2kill <379563000@qq.com>
2024-07-23 17:06:05 +0800

    update config

--- web/ecosystem.config.cjs
+++ web/ecosystem.config.cjs
@@ -2,10 +2,17 @@ module.exports = {
   apps: [
     {
       name: "egg-server_web",
-      port: "8000",
-      exec_mode: "single", // 或者使用 "fork" 如果你希望应用在每个CPU核心上运行一个实例
-      instances: undefined, // 由于使用了single模式，这里不需要设置instances
-      script: "./.output/server/index.mjs",
+      script: "./.output/server/index.mjs", // 应用程序的入口文件路径
+      exec_mode: "fork", // 运行模式，这里使用fork模式
+      instances: 1, // 实例数，根据需要设置
+      watch: false, // 是否监视文件变化并重启应用
+      max_memory_restart: "100M", // 当应用使用内存超过100M时重启
+      env: {
+        PORT: 8000, // 设置环境变量，指定端口号
+      },
+      env_production: {
+        // 生产环境的配置，可以在这里覆盖默认设置
+      },
     },
   ],
 };
