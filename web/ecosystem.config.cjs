82063d01016805634fa8b59918b8e2eb94db8f0c
double2kill <379563000@qq.com>
2024-07-23 17:03:42 +0800

    update ecosystem.config

--- web/ecosystem.config.cjs
+++ web/ecosystem.config.cjs
@@ -3,8 +3,8 @@ module.exports = {
     {
       name: "egg-server_web",
       port: "8000",
-      exec_mode: "cluster",
-      instances: "max",
+      exec_mode: "single", // 或者使用 "fork" 如果你希望应用在每个CPU核心上运行一个实例
+      instances: undefined, // 由于使用了single模式，这里不需要设置instances
       script: "./.output/server/index.mjs",
     },
   ],
